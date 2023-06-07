4bac37105afffb8badca25bbd68d8722335936fd
tramstational <tramtram2205@gmail.com>
2023-06-07 12:22:40 +0700

    Type for component.

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,159 +1,3 @@
-<script>
-export default {
-  data() {
-    return {
-      display: '0',
-      calculatorData: {
-        firstValue: '',
-        operator: '',
-        modValue: '',
-        previousKeyType: '',
-      },
-      keys: [
-        { content: '+', action: 'add' },
-        { content: '-', action: 'subtract' },
-        { content: '×', action: 'multiply' },
-        { content: '÷', action: 'divide' },
-        { content: '7', action: 'number' },
-        { content: '8', action: 'number' },
-        { content: '9', action: 'number' },
-        { content: '4', action: 'number' },
-        { content: '5', action: 'number' },
-        { content: '6', action: 'number' },
-        { content: '1', action: 'number' },
-        { content: '2', action: 'number' },
-        { content: '3', action: 'number' },
-        { content: '0', action: 'number' },
-        { content: '.', action: 'decimal' },
-        { content: 'C', action: 'clear' },
-        { content: '=', action: 'calculate' },
-      ],
-    };
-  },
-  methods: {
-    handleClick(event) {
-      const key = event.target;
-      const displayedNum = this.display;
-      const resultString = this.createResultString(key, displayedNum, this.calculatorData);
-
-      this.display = resultString;
-      this.updateCalculatorState(key, resultString, displayedNum);
-      this.updateVisualState(key);
-    },
-
-    calculate(n1, operator, n2) {
-      const firstNum = parseFloat(n1);
-      const secondNum = parseFloat(n2);
-      if (operator === 'add') return firstNum + secondNum;
-      if (operator === 'subtract') return firstNum - secondNum;
-      if (operator === 'multiply') return firstNum * secondNum;
-      if (operator === 'divide') return firstNum / secondNum;
-    },
-
-    getKeyType(key) {
-      const { action } = key.dataset;
-      if (!action) return 'number';
-      if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-      ) return 'operator';
-      // For everything else, return the action
-      return action;
-    },
-
-    createResultString(key, displayedNum, state) {
-      const keyContent = key.textContent;
-      const keyType = this.getKeyType(key);
-      const {
-        firstValue,
-        operator,
-        modValue,
-        previousKeyType,
-      } = state;
-
-      if (keyType === 'number') {
-        return displayedNum === '0' ||
-          previousKeyType === 'operator' ||
-          previousKeyType === 'calculate'
-          ? keyContent
-          : displayedNum + keyContent;
-      }
-
-      if (keyType === 'decimal') {
-        if (!displayedNum.includes('.')) return displayedNum + '.';
-        if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.';
-        return displayedNum;
-      }
-
-      if (keyType === 'operator') {
-        return firstValue &&
-          operator &&
-          previousKeyType !== 'operator' &&
-          previousKeyType !== 'calculate'
-          ? this.calculate(firstValue, operator, displayedNum)
-          : displayedNum;
-      }
-
-      if (keyType === 'clear') return '0';
-
-      if (keyType === 'calculate') {
-        return firstValue
-          ? previousKeyType === 'calculate'
-            ? this.calculate(displayedNum, operator, modValue)
-            : this.calculate(firstValue, operator, displayedNum)
-          : displayedNum;
-      }
-    },
-
-    updateCalculatorState(key, calculatedValue, displayedNum) {
-      const keyType = this.getKeyType(key);
-      const {
-        firstValue,
-        operator,
-        modValue,
-        previousKeyType,
-      } = this.calculatorData;
-
-      this.calculatorData.previousKeyType = keyType;
-
-      if (keyType === 'operator') {
-        this.calculatorData.operator = key.dataset.action;
-        this.calculatorData.firstValue = firstValue &&
-          operator &&
-          previousKeyType !== 'operator' &&
-          previousKeyType !== 'calculate'
-          ? calculatedValue
-          : displayedNum;
-      }
-
-      if (keyType === 'calculate') {
-        this.calculatorData.modValue = firstValue && previousKeyType === 'calculate'
-          ? modValue
-          : displayedNum;
-      }
-
-      if (keyType === 'clear' && key.textContent === 'AC') {
-        this.calculatorData.firstValue = '';
-        this.calculatorData.modValue = '';
-        this.calculatorData.operator = '';
-        this.calculatorData.previousKeyType = '';
-      }
-    },
-
-    updateVisualState(key) {
-      const keyType = this.getKeyType(key);
-      const keys = Array.from(key.parentNode.children);
-      keys.forEach(k => k.classList.remove('is-depressed'));
-
-      if (keyType === 'operator') key.classList.add('is-depressed');
-      if (keyType === 'clear' && key.textContent !== 'C') key.textContent = 'C';
-    },
-  },
-};
-</script>
-
 <template>
   <div class="container">
     <div class="calculator">
@@ -165,6 +9,183 @@ export default {
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+enum KeyType {
+  Add = 'add',
+  Subtract = 'subtract',
+  Multiply = 'multiply',
+  Divide = 'divide',
+  Decimal = 'decimal',
+  Clear = 'clear',
+  Calculate = 'calculate',
+  Number = 'number',
+  Operator = 'operator'
+}
+
+export default defineComponent({
+  data() {
+    return {
+      display: '0',
+      calculatorData: {
+        firstValue: '',
+        operator: '',
+        modValue: '',
+        previousKeyType: '',
+      },
+      keys: [
+        { content: '+', action: KeyType.Add },
+        { content: '-', action: KeyType.Subtract },
+        { content: '×', action: KeyType.Multiply },
+        { content: '÷', action: KeyType.Divide },
+        { content: '7', action: KeyType.Number },
+        { content: '8', action: KeyType.Number },
+        { content: '9', action: KeyType.Number },
+        { content: '4', action: KeyType.Number },
+        { content: '5', action: KeyType.Number },
+        { content: '6', action: KeyType.Number },
+        { content: '1', action: KeyType.Number },
+        { content: '2', action: KeyType.Number },
+        { content: '3', action: KeyType.Number },
+        { content: '0', action: KeyType.Number },
+        { content: '.', action: KeyType.Decimal },
+        { content: 'C', action: KeyType.Clear },
+        { content: '=', action: KeyType.Calculate },
+      ],
+    };
+  },
+  methods: {
+    handleClick(event: MouseEvent) {
+      const key = event.target as HTMLButtonElement;
+      if (!key) {
+        return;
+      } else {
+        const displayedNum = this.display;
+        const resultString = this.createResultString(key, displayedNum, this.calculatorData);
+
+        this.display = resultString;
+        this.updateCalculatorState(key, resultString, displayedNum);
+      }
+    },
+
+    calculate(n1: string, operator: string, n2: string) {
+      const firstNum = parseFloat(n1);
+      const secondNum = parseFloat(n2);
+      if (operator === KeyType.Add) return firstNum + secondNum;
+      if (operator === KeyType.Subtract) return firstNum - secondNum;
+      if (operator === KeyType.Multiply) return firstNum * secondNum;
+      if (operator === KeyType.Divide) return firstNum / secondNum;
+    },
+
+    getKeyType(key: HTMLElement) {
+      const action = key.dataset.action;
+      if (!action) return KeyType.Number;
+      if (
+        action === KeyType.Add ||
+        action === KeyType.Subtract ||
+        action === KeyType.Multiply ||
+        action === KeyType.Divide
+      ) return KeyType.Operator;
+      // For everything else, return the action
+      return action;
+    },
+
+    createResultString(
+      key: HTMLElement,
+      displayedNum: string,
+      state: {
+        firstValue: string;
+        operator: string;
+        modValue: string;
+        previousKeyType: string;
+      }
+    ) {
+      const keyContent = key.textContent as string;
+      const keyType = this.getKeyType(key);
+      const {
+        firstValue,
+        operator,
+        modValue,
+        previousKeyType,
+      } = state;
+
+      if (keyType === KeyType.Number) {
+        return displayedNum === '0' ||
+          previousKeyType === KeyType.Operator ||
+          previousKeyType === KeyType.Calculate
+          ? keyContent
+          : displayedNum + keyContent;
+      }
+
+      if (keyType === KeyType.Decimal) {
+        if (!displayedNum.includes('.')) return displayedNum + '.';
+        if (previousKeyType === KeyType.Operator || previousKeyType === KeyType.Calculate) return '0.';
+        return displayedNum;
+      }
+
+      if (keyType === KeyType.Operator) {
+        return firstValue &&
+          operator &&
+          previousKeyType !== KeyType.Operator &&
+          previousKeyType !== KeyType.Calculate
+          ? this.calculate(firstValue, operator, displayedNum)
+          : displayedNum;
+      }
+
+      if (keyType === KeyType.Clear) return '0';
+
+      if (keyType === KeyType.Calculate) {
+        return firstValue
+          ? previousKeyType === KeyType.Calculate
+            ? this.calculate(displayedNum, operator, modValue)
+            : this.calculate(firstValue, operator, displayedNum)
+          : displayedNum;
+      }
+    },
+
+    updateCalculatorState(
+      key: { dataset: { action: string }; textContent: string },
+      calculatedValue: string,
+      displayedNum: string
+    ) {
+      const keyType = this.getKeyType(key);
+      const {
+        firstValue,
+        operator,
+        modValue,
+        previousKeyType,
+      } = this.calculatorData;
+
+      this.calculatorData.previousKeyType = keyType;
+
+      if (keyType === KeyType.Operator) {
+        this.calculatorData.operator = key.dataset.action;
+        this.calculatorData.firstValue =
+          firstValue &&
+            operator &&
+            previousKeyType !== KeyType.Operator &&
+            previousKeyType !== KeyType.Calculate
+            ? calculatedValue
+            : displayedNum;
+      }
+
+      if (keyType === KeyType.Calculate) {
+        this.calculatorData.modValue =
+          firstValue && previousKeyType === KeyType.Calculate ? modValue : displayedNum;
+      }
+
+      if (keyType === KeyType.Clear && key.textContent === 'C') {
+        this.calculatorData.firstValue = '';
+        this.calculatorData.modValue = '';
+        this.calculatorData.operator = '';
+        this.calculatorData.previousKeyType = '';
+      }
+    }
+  },
+});
+</script>
 
 <style scoped lang="scss">
 input,
